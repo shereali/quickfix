6ed7235ac013f23a101e0e7d5300c69edc4f49c1
Borhan <borhanjoytech@gmail.com>
2021-01-18 16:58:20 +0600

    branch shift

---
+++ storage/framework/views/a357102f8621d3b034c16b6a17e555964fb7c9bd.php
@@ -0,0 +1,294 @@
+<!doctype html>
+<html dir="ltr" lang="en-US">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <!-- Metas -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="LionCoders" />
+    <!-- Links -->
+    <link rel="icon" type="image/png" href="#" />
+    <!-- google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800" rel="stylesheet">
+    <!-- Plugins CSS -->
+    <link href="<?php echo e(asset('frontend/css/plugin.css')); ?>" rel="stylesheet" />
+    <!-- style CSS -->
+    <link href="<?php echo e(asset('frontend/css/style.css')); ?>" rel="stylesheet" />
+    <!--color switcher css-->
+    <link rel="stylesheet" href="<?php echo e(asset('frontend/css/switcher/skin-aqua.css')); ?>" media="screen" id="style-colors" />
+    <!-- Document Title -->
+    <title>Quickfix - repair service assistant</title>
+    <link rel="shortcut icon" href="<?php echo e(asset('frontend/images/web-logo-icon-01.png')); ?>" type="image/png">
+    <!-- CKEditor link -->
+    <!-- process -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!-- process -->
+</head>
+
+<body style="padding-right: 0px !important;">
+    <div class="preloader js-preloader">
+        <img src="<?php echo e(asset('frontend/images/loader.gif')); ?>">
+    </div>
+    <!--Preloader ends-->
+    <!--Page Wrapper starts-->
+    <div class="page-wrapper fixed-footer">
+        <!--header starts scroll-hide-->
+        <header class="header transparent bg-pn">
+            <!--Main Menu starts-->
+            <div class="site-navbar-wrap v2 p-20">
+                <div class="container">
+                    <div class="site-navbar">
+                        <div class="row align-items-center">
+                            <div class="col-md-4 col-6">
+                                <a class="navbar-brand" href="#"><img src="<?php echo e(asset('frontend/images/logo/logo_dark.png')); ?>" alt="logo" class="img-fluid"></a>
+                            </div>
+                            <div class="col-md-8 col-6">
+                                 <nav class="site-navigation float-right">
+                                    <div class="container">
+                                        <ul class="site-menu js-clone-nav d-none d-lg-block">
+                                            <li>
+                                                
+                                            </li>
+                                            <li>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                                <div class="d-lg-none sm-right">
+                                    <a href="#" class="mobile-bar js-menu-toggle">
+                                        <span class="ion-android-menu"></span>
+                                    </a>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <!--mobile-menu starts -->
+                    <div class="site-mobile-menu">
+                        <div class="site-mobile-menu-header">
+                            <div class="site-mobile-menu-close  js-menu-toggle">
+                                <span class="ion-ios-close-empty"></span>
+                            </div>
+                        </div>
+                        <div class="site-mobile-menu-body"></div>
+                    </div>
+                    <!--mobile-menu ends-->
+                </div>
+            </div>
+            <!--Main Menu ends-->
+        </header>
+        <!--Header ends-->
+        <div id="app_frontend">
+            
+        </div>    
+        <!-- Scroll to top starts-->
+        <span class="scrolltotop"><i class="ion-arrow-up-c"></i></span>
+        <!-- Scroll to top ends-->
+    </div>
+    <!--Page Wrapper ends-->
+    <!--Footer Starts-->
+    <div class="footer-wrapper no-pad-tb footer-fixed v1 bg-pn">
+       <div class="overlay op-1"></div>
+        <div class="footer-top-area section-padding" style="padding:45px 0px !important;">
+            <div class="container">
+                <div class="row nav-folderized">
+                    <div class="col-lg-4 col-md-12">
+                        <div class="footer-logo">
+                            <a href="index.html"> <img src="<?php echo e(asset('frontend/images/footer_logo.png')); ?>" alt="logo"></a>
+                            <div class="company-desc">
+                                <p>
+                                We provide quick service process at our Store. Quickfix is one of the best reasonable mobile phone and electronic gadget service platform, we are fixing all models of smartphones, laptops and other electronic gadgets.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12">
+                        <div class="footer-content nav">
+                            <h2 class="title">Helpful Links</h2>
+                            <ul class="list">
+                                <li><a class="link-hov style2" href="#">Login</a></li>
+                                <li><a class="link-hov style2" href="#">My Account</a></li>
+                                <li><a class="link-hov style2" href="#">Add Listing</a></li>
+                                <li><a class="link-hov style2" href="#">Privacy policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- <div class="col-lg-3 col-md-12">
+                        <div class="footer-content nav">
+                            <h2 class="title">Category</h2>
+                            <ul class="list">
+                                <li><a class="link-hov style2" href="#">Shop</a></li>
+                                <li><a class="link-hov style2" href="#">Travel</a></li>
+                                <li><a class="link-hov style2" href="#">Sport</a></li>
+                                <li><a class="link-hov style2" href="#">Restaurant</a></li>
+                            </ul>
+                        </div>
+                    </div> -->
+                    <div class="col-lg-4 col-md-12">
+                        <div class="footer-content nav">
+                            <h2 class="title">Contact us</h2>
+                            <ul class="list footer-list">
+                                <li>
+                                    <div class="contact-info">
+                                        <div class="icon">
+                                            <i class="ion-ios-location"></i>
+                                        </div>
+                                        <div class="text">House-44, Road-2, Block-B, Niketan, Gulshan-1, Dhaka-1212.</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="contact-info">
+                                        <div class="icon">
+                                            <i class="ion-email"></i>
+                                        </div>
+                                        <div class="text"><a href="#">support@quickfix.com.bd</a></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="contact-info">
+                                        <div class="icon">
+                                            <i class="ion-ios-telephone"></i>
+                                        </div>
+                                        <div class="text">Hotline 09642363636</div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <ul class="social-buttons style2">
+                                <li><a href="https://www.facebook.com/quickfixbd" target=_blank><i class="ion-social-facebook"></i></a></li>
+                                <li><a href="https://twitter.com/quickfixservice" target=_blank><i class="ion-social-twitter"></i></a></li>
+                                <li><a href="https://www.linkedin.com/company/quickfixbd/" target=_blank><i class="ion-social-linkedin"></i></a></li>
+                                <li><a href="https://www.youtube.com/channel/UCW4gvFtndv5iUHkQoJ_zHTw" target=_blank><i class="ion-social-youtube"></i></a></li>
+                                <!-- <li><a href="https://www.behance.net/tamimkhan22" target=_blank><i class="ion-social-behance"></i</a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2 text-center">
+                        <p>Copyright</p>
+                    </div><hr>
+                    <div class="col-md-8 offset-md-2 text-center">
+                        <p>&copy; 2016-2020 Quickfix Service Ltd.| Developed by Joy Technologies Limited.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>
+    
+    $('.option').on('click', function(){
+        var item = $(this).attr('value');
+            if(item == 'laptop')
+        {
+            document.getElementById("problems_target_laptop").style.display = "block";
+            document.getElementById("problems_target_mobile").style.display = "none";
+        }else if(item == 'mobile') {
+            document.getElementById("problems_target_mobile").style.display = "block";
+            document.getElementById("problems_target_laptop").style.display = "none";
+        }
+        
+        console.log(item);
+    });
+
+    $(document).ready(function(){
+                       
+            var counter = 1;
+                
+            $("#addButton").click(function () {
+                var el = document.getElementById("removeButton");     
+                el.disabled = false; 
+            if(counter>10){
+                    alert("Only 10 textboxes allow");
+                    return false;
+            }   
+                
+            var newTextBoxDiv = $(document.createElement('div'))
+                .attr("id", 'TextBoxDiv' + counter);
+            
+                        
+            newTextBoxDiv.after().html('<input type="text" style="margin-top:10px" class="form-control filter-input" name="textbox' + counter + 
+                '" id="textbox' + counter + '" placeholder="write your problem" >');
+              
+                    
+            newTextBoxDiv.appendTo("#TextBoxesGroup");
+    
+                        
+            counter++;
+            });
+    
+            $("#removeButton").click(function () {
+            if(counter==1){
+                el.disabled = true; 
+                alert("No more textbox to remove");
+                return false;
+            }   
+                
+            counter--;
+                    
+                $("#TextBoxDiv" + counter).remove();
+                    
+            });
+                
+           
+            });
+            
+    
+                var i = 0;
+    
+                $('#myButton').click(function() {
+                    i++;
+                   
+    
+                var id='#myDiv'+i
+    
+                console.log(id);
+                $(id).append('<input type="text" class="form-control filter-input" placeholder="e.g.Display Broken">');
+    
+    
+    
+    
+            });
+            
+                    $(document).ready(function() {
+                $('#check-g').change(function() {
+    
+                    $('#mycheckboxdiv').toggle();
+                });
+            });
+
+
+            
+       
+  
+            // ClassicEditor
+            //     .create( document.querySelector( '#editor' ) )
+            //     .catch( error => {
+            //         console.error( error );
+            //     } );
+
+        </script>
+        <!--Footer ends-->
+        
+        <!--Scripts starts-->
+        <!--plugin js-->
+        
+        <script src="<?php echo e(asset('frontend/js/plugin.js')); ?>"></script>
+        <!--google maps-->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_8C7p0Ws2gUu7wo0b6pK9Qu7LuzX2iWY&amp;libraries=places&amp;callback=initAutocomplete"></script>
+        <!--Main js-->
+        <script src="<?php echo e(asset('frontend/js/main.js')); ?>"></script>
+        <!--Scripts ends-->
+    
+        <script src="<?php echo e(asset('js/app_frontend.js')); ?>" defer></script>
+</body>
+</html>
+<?php /**PATH G:\laragon\www\quickfix_web_v1\resources\views/frontend/layouts/master.blade.php ENDPATH**/ ?>
